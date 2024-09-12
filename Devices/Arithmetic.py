8737ab6e77902138e875d44ef2ffc67f978715c1
Soonkyu <reingel@gmail.com>
2024-09-12 19:05:47 +0900

    Implemented FullAdder

--- Devices/Arithmetic.py
+++ Devices/Arithmetic.py
@@ -1,7 +1,7 @@
 import unittest
 from BitValue import *
 from SimulatedCircuit import SimulatedCircuit
-from Gate import And, Xor
+from Gate import And, Or, Xor
 from Junction import Split
 
 
@@ -9,7 +9,7 @@ class HalfAdder(SimulatedCircuit):
     def __init__(self, name):
         self.device_name = 'HalfAdder'
 
-        # creat update_sequence
+        # create elements
         self.spl1 = Split('spl1')
         self.spl2 = Split('spl2')
         self.xor = Xor('xor')
@@ -27,13 +27,47 @@ class HalfAdder(SimulatedCircuit):
         self.S = self.xor.out
         self.CO = self.and1.out
 
-        # update sequences
+        # update sequence
         self.update_sequence = [self.spl1, self.spl2, self.xor, self.and1]
     
         super().__init__('HalfAdder', name)
 
     def __repr__(self):
-        str = f'   {self.device_name}({self.name}, [A B S CO] = [{self.A.value} {self.B.value} {self.S.value} {self.CO.value}]'
+        str = f'   {self.device_name}({self.name}, [A B] -> [S CO] = [{self.A.value} {self.B.value}] -> [{self.S.value} {self.CO.value}]'
+        # str += '\n'
+        # for device in self.update_sequence:
+        #     str += f'      {device}\n'
+        return str
+
+
+class FullAdder(SimulatedCircuit):
+    def __init__(self, name):
+        self.device_name = 'FullAdder'
+
+        # create elements
+        self.ha1 = HalfAdder('ha1')
+        self.ha2 = HalfAdder('ha2')
+        self.or1 = Or('or1')
+
+        # connect
+        self.ha1.S >> self.ha2.B
+        self.ha2.CO >> self.or1.in1
+        self.ha1.CO >> self.or1.in2
+
+        # create access points
+        self.CI = self.ha2.A
+        self.A = self.ha1.A
+        self.B = self.ha1.B
+        self.S = self.ha2.S
+        self.CO = self.or1.out
+
+        # update sequence
+        self.update_sequence = [self.ha1, self.ha2, self.or1]
+
+        super().__init__('FullAdder', name)
+
+    def __repr__(self):
+        str = f'   {self.device_name}({self.name}, [CI A B] -> [S CO] = [{self.CI.value} {self.A.value} {self.B.value}] -> [{self.S.value} {self.CO.value}]'
         # str += '\n'
         # for device in self.update_sequence:
         #     str += f'      {device}\n'
@@ -51,11 +85,27 @@ class TestArithmetic(unittest.TestCase):
             for in2 in [OPEN, HIGH]:
                 ha.A.value = in1
                 ha.B.value = in2
-                ha.step(n=2)
+                ha.step()
                 print(ha)
                 self.assertEqual(ha.S.value, truth_table_S[in1][in2])
                 self.assertEqual(ha.CO.value, truth_table_CO[in1][in2])
 
+    def test_full_adder(self):
+        fa = FullAdder('fa1')
+        fa.power_on()
+        fa.step()
+        truth_table_S = [[[0, 1], [1, 0]], [[1, 0], [0, 1]]]
+        truth_table_CO = [[[0, 0], [0, 1]], [[0, 1], [1, 1]]]
+        for ci in [OPEN, HIGH]:
+            for in1 in [OPEN, HIGH]:
+                for in2 in [OPEN, HIGH]:
+                    fa.CI.value = ci
+                    fa.A.value = in1
+                    fa.B.value = in2
+                    fa.step()
+                    print(fa)
+                    self.assertEqual(fa.S.value, truth_table_S[ci][in1][in2])
+                    self.assertEqual(fa.CO.value, truth_table_CO[ci][in1][in2])
 
 
 if __name__ == '__main__':
