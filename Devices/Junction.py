377bcd5c64c6acfbe726b31fdacd19b4213d3daf
Soonkyu <reingel@gmail.com>
2024-11-22 14:24:46 +0900

    Updated Branch

--- Devices/Junction.py
+++ Devices/Junction.py
@@ -1,4 +1,3 @@
-import unittest
 from BitValue import *
 from SimulatedCircuit import SimulatedCircuit
 from Port import Port
@@ -14,52 +13,54 @@ class Branch(SimulatedCircuit):
         self.device_name = 'Branch'
         self.name = name
 
-        self._state = OPEN
-        self._nI = 0
-        self._nO = 0
+        self._value = OPEN
+        self._ninport = 0
+        self._noutport = 0
 
-        self.I = []
-        self.O = []
+        self.inport = []
+        self.outport = []
 
         super().__init__(self.device_name, self.name)
     
     def __repr__(self):
-        return f'Branch({self.name}, {[strof(p.value) for p in self.I]} -> {strof(self.state)} -> {[strof(p.value) for p in self.O]})'
+        return f'Branch({self.name}, {[strof(p.value) for p in self.inport]} -> {strof(self.value)} -> {[strof(p.value) for p in self.outport]})'
     
     @property
-    def state(self):
-        return self._state
+    def value(self):
+        return self._value
     
     @property
-    def nI(self):
-        return self._nI
+    def ninport(self):
+        return self._ninport
     
     @property
-    def nO(self):
-        return self._nO
+    def noutport(self):
+        return self._noutport
     
-    @property
-    def is_not_empty(self):
-        return self.nI > 0 and self.nO > 0
-
+    def set(self): # In case of self.inport is empty, self acts like Port
+        self._value = HIGH
+    
+    def reset(self): # In case of self.inport is empty, self acts like Port
+        self._value = OPEN
+    
     def add_inport(self, port):
         if isinstance(port, Port):
-            n = self.nI
-            p = Port(f'I{n + 1}', self)
+            n = self.ninport
+            p = Port(f'inport{n + 1}', self)
             port >> p
-            self.I.append(p)
-            self._nI += 1
+            self.inport.append(p)
+            self._ninport += 1
             return self
         else:
             raise(RuntimeError)
 
     def add_outport(self, port):
         if isinstance(port, Port):
-            n = self.nO
-            p = Port(f'O{n + 1}', self)
+            n = self.noutport
+            p = Port(f'outport{n + 1}', self)
             p >> port
-            self.O.append(p)
-            self._nO += 1
+            self.outport.append(p)
+            self._noutport += 1
         else:
             raise(RuntimeError)
     
@@ -73,30 +74,27 @@ class Branch(SimulatedCircuit):
         return self.__lshift__(port)
     
     def update_inport(self):
-        for p in self.I:
+        for p in self.inport:
             p.update_value()
 
     def update_state(self):
-        if self.nI == 0:
-            return
-
-        values = set([p.value for p in self.I])
+        values = set([p.value for p in self.inport])
         if HIGH in values and GND in values:
             # print('Short circuit !!!')
             raise(NotImplementedError)
         elif HIGH in values:
-            self._state = HIGH
+            self._value = HIGH
         elif GND in values:
-            self._state = GND
+            self._value = GND
         else:
-            self._state = OPEN
+            self._value = OPEN
 
-        for p in self.I:
-            p.value = self.state
+        for p in self.inport:
+            p.value = self.value
 
     def calc_output(self):
-        for p in self.O:
-            p.value = self.state
+        for p in self.outport:
+            p.value = self.value
 
 
 class Junction(SimulatedCircuit):
@@ -161,6 +159,10 @@ class Split8(Junction):
 
 
 
+
+
+import unittest
+
 class TestConnection(unittest.TestCase):
     def test_branch(self):
         print('test_branch')
@@ -175,13 +177,11 @@ class TestConnection(unittest.TestCase):
         q3 = Port('q3', dev1)
 
         brn = Branch('brn1')
-        self.assertEqual(brn.is_not_empty, False)
         p1 >> brn >> q1
         p2 >> brn >> q2
         p3 >> brn >> q3
-        self.assertEqual(brn.nI, 3)
-        self.assertEqual(brn.nO, 3)
-        self.assertEqual(brn.is_not_empty, True)
+        self.assertEqual(brn.ninport, 3)
+        self.assertEqual(brn.noutport, 3)
 
         SHORT_CIRCUIT = 99
         io = [
@@ -225,13 +225,92 @@ class TestConnection(unittest.TestCase):
                 q1.update_value()
                 q2.update_value()
                 q3.update_value()
-                # print(brn, q1, q2, q3)
-                self.assertEqual(brn.state, io[i][1])
-                self.assertEqual(q1.value, brn.state)
-                self.assertEqual(q2.value, brn.state)
-                self.assertEqual(q3.value, brn.state)
+                # print(strof(p1.value), strof(p2.value), strof(p3.value), end=' -> ')
+                # print(strof(q1.value), strof(q2.value), strof(q3.value))
+                # print(brn)
+                self.assertEqual(brn.value, io[i][1])
+                self.assertEqual(q1.value, brn.value)
+                self.assertEqual(q2.value, brn.value)
+                self.assertEqual(q3.value, brn.value)
             except NotImplementedError:
                 self.assertEqual(io[i][1], SHORT_CIRCUIT)
+    
+    def test_branch_no_input(self):
+        print('test_branch_no_input')
+        
+        from Gate import Buffer
+
+        class Dev(SimulatedCircuit):
+            def __init__(self, name):
+                self.brn = Branch('brn')
+                self.bf = [Buffer('bf0'), Buffer('bf1')]
+
+                self.brn >> self.bf[0].I
+                self.brn >> self.bf[1].I
+
+                self.update_sequence = [self.brn, self.bf[0], self.bf[1]]
+
+                self.I = self.brn
+                self.O = [self.bf[0].O, self.bf[1].O]
+            
+                super().__init__('Dev', name)
+        
+        bf1 = Buffer('bf1')
+        dev = Dev('dev')
+        bf2 = Buffer('bf2')
+        bf1.power_on()
+        dev.power_on()
+        bf2.power_on()
+
+        bf1.O >> dev.I
+        dev.O[0] >> bf2.I
+
+        bf1.I.set()
+
+        bf1.step()
+        dev.step()
+        bf2.step()
+
+        self.assertEqual(bf2.O.value, HIGH)
+    
+    def test_branch_no_output(self):
+        print('test_branch_no_output')
+        
+        from Gate import Buffer
+
+        class Dev(SimulatedCircuit):
+            def __init__(self, name):
+                self.bf = [Buffer('bf0'), Buffer('bf1')]
+                self.brn = Branch('brn')
+
+                self.bf[0].O >> self.brn
+                self.bf[1].O >> self.brn
+
+                self.update_sequence = [self.bf[0], self.bf[1], self.brn]
+
+                self.I = [self.bf[0].I, self.bf[1].I]
+                self.O = self.brn
+            
+                super().__init__('Dev', name)
+        
+        bf1 = Buffer('bf1')
+        dev = Dev('dev')
+        bf2 = Buffer('bf2')
+        bf1.power_on()
+        dev.power_on()
+        bf2.power_on()
+
+        bf1.O >> dev.I[0]
+        dev.O >> bf2.I
+
+        bf1.I.set()
+
+        bf1.step()
+        dev.step()
+        bf2.step()
+
+        self.assertEqual(bf2.O.value, HIGH)
+    
 
 
     def test_split(self):
